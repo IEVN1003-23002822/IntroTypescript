309feae21744ede64efa15cc6c454d6b6568d70e
KevinElias <86795@alumnos.utleon.edu.mx>
2026-09-04 22:36:16 -0600

    Tiposdedatos04092026

---
+++ src/TemasTS/01-tipos-bas.ts
@@ -0,0 +1,17 @@
+console.log("Hola mundo")
+
+var num1=23;
+let num2=65;
+const num3=22;
+
+let nombre:string
+let num:number
+let activo:boolean
+let sum:number=num1 + num2 + num3
+console.log("La suma es:"+sum)
+
+nombre="pedro";
+console.log("nombre");
+
+console.log("nombre");
+console.log("prueba");
